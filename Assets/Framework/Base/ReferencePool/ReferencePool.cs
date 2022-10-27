48d689444d4b7a303b741cbdd32f524b2e857bd8
xiongtianya <xiongtianya@xunlei.com>
2022-10-27 21:32:45 +0800

    ReferencePool、Fsm

--- Assets/Framework/Base/ReferencePool/ReferencePool.cs
+++ Assets/Framework/Base/ReferencePool/ReferencePool.cs
@@ -1,7 +1,151 @@
+using System;
+using System.Collections.Generic;
 namespace XGameFramework
 {
     public static partial class ReferencePool
     {
+        private static readonly Dictionary<Type, ReferenceCollection> s_ReferenceCollections = new Dictionary<Type, ReferenceCollection>();
+        private static bool m_EnableStricCheck = false;
+
+        public static bool EnableStricCheck
+        {
+            get
+            {
+                return m_EnableStricCheck;
+            }
+            set
+            {
+                m_EnableStricCheck = value;
+            }
+        }
+
+        public static int Count
+        {
+            get
+            {
+                return s_ReferenceCollections.Count;
+            }
+        }
+
+        public static ReferencePoolInfo[] GetAllReferencePoolInfo()
+        {
+            int index = 0;
+            ReferencePoolInfo[] resules = null;
+            lock (s_ReferenceCollections)
+            {
+                resules = new ReferencePoolInfo[s_ReferenceCollections.Count];
+                foreach (KeyValuePair<Type, ReferenceCollection> referenceCollection in s_ReferenceCollections)
+                {
+                    resules[index++] = new ReferencePoolInfo(referenceCollection.Key, referenceCollection.Value.UnusedReferenceCount, referenceCollection.Value.UsingReferenceCount, referenceCollection.Value.AcquireReferenceCount, referenceCollection.Value.ReleaseReferenceCount, referenceCollection.Value.AddReferenceCount, referenceCollection.Value.RemoveReferenceCount);
+
+                }
+            }
+            return resules;
+        }
+
+        public static void ClearAll()
+        {
+            lock (s_ReferenceCollections)
+            {
+                foreach (KeyValuePair<Type, ReferenceCollection> referenceCollection in s_ReferenceCollections)
+                {
+                    referenceCollection.Value.RemoveAll();
+                }
+                s_ReferenceCollections.Clear();
+            }
+        }
+
+        public static T Acquire<T>() where T : class, IReference, new()
+        {
+            return GetReferenceCollection(typeof(T)).Acquire<T>();
+        }
+
+        public static IReference Acquire(Type referenceType)
+        {
+            return GetReferenceCollection(referenceType).Acquire();
+        }
+
+        public static void Release(IReference reference)
+        {
+            if (reference == null)
+            {
+                throw new GameFrameworkException("Reference is invalid.");
+            }
+
+            Type referenceType = reference.GetType();
+            InternalCheckReferenceType(referenceType);
+            GetReferenceCollection(referenceType).Release(reference);
+        }
+
+        public static void Add<T>(int count) where T : class, IReference, new()
+        {
+            GetReferenceCollection(typeof(T)).Add<T>(count);
+        }
+
+        public static void Add(Type referenceType, int count)
+        {
+            InternalCheckReferenceType(referenceType);
+            GetReferenceCollection(referenceType).Add(count);
+        }
+
+        public static void Remove<T>(int count) where T : class, IReference
+        {
+            GetReferenceCollection(typeof(T)).Remove(count);
+        }
+
+        public static void Remove(Type referenceType, int count)
+        {
+            InternalCheckReferenceType(referenceType);
+            GetReferenceCollection(referenceType).Remove(count);
+        }
+
+        public static void RemoveAll<T>() where T : class, IReference
+        {
+            GetReferenceCollection(typeof(T)).RemoveAll();
+        }
+
+        public static void RemoveAll(Type referenceType)
+        {
+            InternalCheckReferenceType(referenceType);
+            GetReferenceCollection(referenceType).RemoveAll();
+        }
+
+        private static void InternalCheckReferenceType(Type referenceType)
+        {
+            if (!m_EnableStricCheck)
+            {
+                return;
+            }
+            if (referenceType == null)
+            {
+                throw new GameFrameworkException("Reference type is invalid.");
+            }
+            if (!referenceType.IsClass || referenceType.IsAbstract)
+            {
+                throw new GameFrameworkException("Reference type is not a non-abstract class type.");
+            }
+            if (!typeof(IReference).IsAssignableFrom(referenceType))
+            {
+                throw new GameFrameworkException(string.Format("Reference type '{0}' is invalid.", referenceType.FullName));
+            }
+        }
+
+        private static ReferenceCollection GetReferenceCollection(Type referenceType)
+        {
+            if (referenceType == null)
+            {
+                throw new GameFrameworkException("ReferenceType is invalid.");
+            }
+            ReferenceCollection referenceCollection = null;
+            lock (s_ReferenceCollections)
+            {
+                if (!s_ReferenceCollections.TryGetValue(referenceType, out referenceCollection))
+                {
+                    referenceCollection = new ReferenceCollection(referenceType);
+                    s_ReferenceCollections.Add(referenceType, referenceCollection);
+                }
+            }
+        }
 
     }
 }
--- Assets/Framework/Fsm/Fsm.cs
+++ Assets/Framework/Fsm/Fsm.cs
@@ -85,6 +85,7 @@ namespace XGameFramework.Fsm
             }
         }
 
+
         public static Fsm<T> Create(string name, T owner, params FsmState<T>[] states)
         {
             if (owner == null)
@@ -98,19 +99,19 @@ namespace XGameFramework.Fsm
             }
 
             Fsm<T> fsm = new Fsm<T>();
-            fsm.CurrentStateName = name;
+            fsm.Name = name;
             fsm.m_Owner = owner;
             fsm.m_IsDestroyed = false;
             foreach (FsmState<T> state in states)
             {
                 if (state == null)
                 {
-                    Debug.LogError("FSM states is invalid.");
+                    throw new GameFrameworkException("FSM states is invalid.");
                 }
                 Type stateType = state.GetType();
                 if (fsm.m_States.ContainsKey(stateType))
                 {
-                    Debug.LogError(Utility.Text.Format("FSM '{0}' state '{1}' is already exist.", new TypeNamePair(typeof(T), name), stateType.FullName));
+                    throw new GameFrameworkException(Utility.Text.Format("FSM '{0}' state '{1}' is already exist.", new TypeNamePair(typeof(T), name), stateType.FullName));
                 }
                 fsm.m_States.Add(stateType, state);
                 state.OnInit(fsm);
@@ -175,9 +176,261 @@ namespace XGameFramework.Fsm
                     {
                         continue;
                     }
+                    ReferencePool.Release(data.Value);
                 }
+                m_Datas.Clear();
+            }
+            m_CurrentState = null;
+            m_CurrentStateTime = 0f;
+            m_IsDestroyed = true;
+        }
+
+        public void Start<TState>() where TState : FsmState<T>
+        {
+            if (IsRunning)
+            {
+                throw new GameFrameworkException("FSM is running, can not start again.");
+            }
+
+            FsmState<T> state = GetState<TState>();
+            if (state == null)
+            {
+                throw new GameFrameworkException(Utility.Text.Format("FSM '{0}' can not start state '{1}' which is not exist.", new TypeNamePair(typeof(T), Name), typeof(TState).FullName));
+            }
+            m_CurrentStateTime = 0f;
+            m_CurrentState = state;
+            m_CurrentState.OnEnter(this);
+        }
+
+        public void Start(Type stateType)
+        {
+            if (IsRunning)
+            {
+                throw new GameFrameworkException("FSM is running, can not start again.");
+            }
+
+            if (stateType == null)
+            {
+                throw new GameFrameworkException("State type is invalid.");
+            }
+
+            if (!typeof(FsmState<T>).IsAssignableFrom(stateType))
+            {
+                throw new GameFrameworkException(Utility.Text.Format("State type '{0}' is invalid.", stateType.FullName));
+            }
+            FsmState<T> state = GetState(stateType);
+            if (state == null)
+            {
+                throw new GameFrameworkException(Utility.Text.Format("FSM '{0}' can not start state '{1}' which is not exist.", new TypeNamePair(typeof(T), Name), typeof(TState).FullName));
+            }
+            m_CurrentStateTime = 0f;
+            m_CurrentState = state;
+            m_CurrentState.OnEnter(this);
+        }
+
+        public bool HasState<TState>() where TState : FsmState<T>
+        {
+            return m_States.ContainsKey(typeof(TState));
+        }
+
+        public bool HasState(Type stateType)
+        {
+            if (stateType == null)
+            {
+                throw new GameFrameworkException("State type is invalid.");
+            }
+            if (!typeof(FsmState<T>).IsAssignableFrom(stateType))
+            {
+                throw new GameFrameworkException(Utility.Text.Format("State type '{0}' is invalid.", stateType.FullName));
+            }
+            return m_States.ContainsKey(stateType);
+        }
+
+
+        public TState GetState<TState>() where TState : FsmState<T>
+        {
+            FsmState<T> state = null;
+            if (m_States.TryGetValue(typeof(TState), out state))
+            {
+                return (TState)state;
+            }
+            return null;
+        }
+
+        public FsmState<T> GetState(Type stateType)
+        {
+            if (stateType == null)
+            {
+                throw new GameFrameworkException("State type is invalid.");
+            }
+
+            if (!typeof(FsmState<T>).IsAssignableFrom(stateType))
+            {
+                throw new GameFrameworkException(Utility.Text.Format("State type '{0}' is invalid.", stateType.FullName));
+            }
+
+            FsmState<T> state = null;
+            if (m_States.TryGetValue(stateType, out state))
+            {
+                return state;
+            }
+
+            return null;
+        }
+
+        public FsmState<T>[] GetAllStates()
+        {
+            int index = 0;
+            FsmState<T>[] results = new FsmState<T>(m_States.Count);
+            foreach (KeyValuePair<Type, FsmState<T>> state in m_States)
+            {
+                results[index++] = state.Value;
+            }
+            return results;
+            //为什么不直接Values()
+            //return m_States.Values();
+        }
+
+        public void GetAllStates(List<FsmState<T>> results)
+        {
+            if (results == null)
+            {
+                throw new GameFrameworkException("Results is invalid.");
+            }
+
+            results.Clear();
+            foreach (KeyValuePair<Type, FsmState<T>> state in m_States)
+            {
+                results.Add(state.Value);
             }
         }
 
+        public bool HasData(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new GameFrameworkException("Data name is invalid.");
+            }
+
+            if (m_Datas == null)
+            {
+                return false;
+            }
+
+            return m_Datas.ContainsKey(name);
+        }
+
+        public TData GetData<TData>(string name) where TData : Variable
+        {
+            return (TData)GetData(name);
+        }
+
+        public Variable GetData(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new GameFrameworkException("Data name is invalid.");
+            }
+
+            if (m_Datas == null)
+            {
+                return null;
+            }
+
+            Variable data = null;
+            if (m_Datas.TryGetValue(name, out data))
+            {
+                return data;
+            }
+
+            return null;
+        }
+
+        public void SetData<TData>(string name, TData data) where TData : Variable
+        {
+            SetData(name, (Variable)data);
+        }
+
+        public void SetData(string name, Variable data)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new GameFrameworkException("Data name is invalid.");
+            }
+
+            if (m_Datas == null)
+            {
+                m_Datas = new Dictionary<string, Variable>(StringComparer.Ordinal);
+            }
+
+            Variable oldData = GetData(name);
+            if (oldData != null)
+            {
+                ReferencePool.Release(oldData);
+            }
+
+            m_Datas[name] = data;
+        }
+
+        public bool RemoveData(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new GameFrameworkException("Data name is invalid.");
+            }
+
+            if (m_Datas == null)
+            {
+                return false;
+            }
+
+            Variable oldData = GetData(name);
+            if (oldData != null)
+            {
+                ReferencePool.Release(oldData);
+            }
+
+            return m_Datas.Remove(name);
+        }
+
+        internal override void Update(float elapseSeconds, float realElapseSeconds)
+        {
+            if (m_CurrentState == null)
+            {
+                return;
+            }
+
+            m_CurrentStateTime += elapseSeconds;
+            m_CurrentState.OnUpdate(this, elapseSeconds, realElapseSeconds);
+        }
+
+        internal override void Shutdown()
+        {
+            ReferencePool.Release(this);
+        }
+
+        internal void ChangeState<TState>() where TState : FsmState<T>
+        {
+            ChangeState(typeof(TState));
+        }
+
+        internal void ChangeState(Type stateType)
+        {
+            if (m_CurrentState == null)
+            {
+                throw new GameFrameworkException("Current state is invalid.");
+            }
+
+            FsmState<T> state = GetState(stateType);
+            if (state == null)
+            {
+                throw new GameFrameworkException(Utility.Text.Format("FSM '{0}' can not change state to '{1}' which is not exist.", new TypeNamePair(typeof(T), Name), stateType.FullName));
+            }
+
+            m_CurrentState.OnLeave(this, false);
+            m_CurrentStateTime = 0f;
+            m_CurrentState = state;
+            m_CurrentState.OnEnter(this);
+        }
     }
 }
